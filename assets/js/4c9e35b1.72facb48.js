d5eac7edef8dcb39e4cb00f88b8d687fbcc012cd
cruxcode <swaroopshyam0@gmail.com>
2022-07-15 13:06:14 +0530

    Deploy website - based on 66d5d4236a6d7ce62647e5ee121a84235adcaab4

---
+++ assets/js/4c9e35b1.72facb48.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkatriframework=self.webpackChunkatriframework||[]).push([[9035],{499:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/hola","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/e16015ca.9dad1e03.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkatriframework=self.webpackChunkatriframework||[]).push([[9700],{5688:a=>{a.exports=JSON.parse('{"label":"hola","permalink":"/blog/tags/hola","allTagsPath":"/blog/tags","count":1}')}}]);
